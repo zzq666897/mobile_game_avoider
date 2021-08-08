47f46ac1a8bf234b6d85f6ea1877092f0beeb479
RookieDavee <zzq666897@gmail.com>
2021-08-08 00:48:48 -0500

    Ver1.0.1

---
+++ Game_Avoider/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other) 
+    {
+       PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+
+       if(playerHealth == null){return;}
+      
+       
+          playerHealth.Crash();
+       
+    }
+}
---
+++ Game_Avoider/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+
+
+   public void Crash()
+   {
+       gameObject.SetActive(false);
+   }
+}
